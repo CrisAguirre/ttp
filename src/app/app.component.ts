785aee51215f3570ce0ea390d83c2640ad88bd0e
Cris <krontroth@gmail.com>
2026-03-24 12:46:50 -0500

    loader + servicios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   menuOpen = false;
-  expanded: boolean[] = [false, false, false];
+  expanded: boolean[] = [false, false, false, false];
+  isLoaded = false;
+
+  ngOnInit(): void {
+    // Hide loader after 2.2s (2s animation + 0.2s buffer for fade)
+    setTimeout(() => {
+      this.isLoaded = true;
+    }, 2200);
+  }
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
